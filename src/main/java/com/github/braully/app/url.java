030ba0a37b09ae49e6656155e54ec92aaba1af9b
Braully Rocha <braully@gmail.com>
2019-05-28 20:25:02 -0300

    boleto, o inicio.

--- src/main/java/com/github/braully/app/url.java
+++ src/main/java/com/github/braully/app/url.java
@@ -53,6 +53,7 @@ public class url {
         rewrite_forward.put("/system/report", "/jsf/app/report.xhtml");
         rewrite_forward.put("/system/settings", "/jsf/app/settings.xhtml");
         rewrite_forward.put("/system/import", "/mb/import");
-    }
 
+        rewrite_forward.put("/entity-crud-scratch", "/jsf/entity-crud-scratch.xhtml");
+    }
 }
